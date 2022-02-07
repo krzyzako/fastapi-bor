5666770132fb64a26de9aea3c78e4f3b07ae1959
Łukasz Lindstedt <krzyzako@o2.pl>
2022-02-07 10:56:08 +0100

    Add auth 1/2

--- app/service/home/krzyzak/Dokumenty/docker/backend/app/service/mqtt.py/my-site/static/nuxt-error.vue
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fmy-site\u002Fstatic\u002F1643965575",layout:"default",error:a,state:a,serverRendered:false,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002Fmy-site\u002F",cdnURL:a}}}}(null,"\u002F"));
